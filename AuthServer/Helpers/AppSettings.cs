bd623698e1817fb6cc72b62b51e915fb3c535367
raphael00fellipe@gmail.com <z33p>
2019-11-28 09:16:26 -0300

    starting to build authentication server

---
+++ AuthServer/Helpers/AppSettings.cs
@@ -0,0 +1,7 @@
+namespace AuthServer.Helpers
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+    }
+}
---
+++ AuthServer/Models/AuthenticateModel.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AuthServer.Models
+{
+    public class AuthenticateModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
