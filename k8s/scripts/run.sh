a7a144a201f941bd2b3cd570b1159f4d14e16fd9
z33p <raphael00fellipe@gmail.com>
2020-05-10 18:27:31 -0300

    k8s

---
+++ k8s/scripts/run.sh
@@ -0,0 +1,8 @@
+kubectl apply -f ../secret.yml
+kubectl apply -f ../database.yml
+kubectl apply -f ../authserver.yml
+kubectl apply -f ../neptune.yml
+kubectl apply -f ../uranus.yml
+kubectl apply -f ../frontend.yml
+
+
