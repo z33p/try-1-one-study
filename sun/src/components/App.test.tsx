9e9c6a12db9b9baa9c01c9b4b7bc2c0f72da1f7d
z33p <raphael00fellipe@gmail.com>
2020-05-13 20:53:29 -0300

    Wip(SUN): a simple input and button to show notebook created in state
    Adds a visual representation of the change of state when adding a notebook
    
    BC: SUN microservice still unavailable

--- sun/src/components/App.test.tsx
+++ sun/src/components/App.test.tsx
@@ -1,9 +1,30 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import Enzyme, { mount } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+import { Provider } from "react-redux";
+import store from "../redux/store";
 import App from "./App";
 
-test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+describe("<App /> unit test", () => {
+  const getWrapper = () =>
+    mount(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+
+  it("should add a notebook with given input value as title", () => {
+    const wrapper = getWrapper();
+
+    const title = "Hello";
+
+    expect(wrapper.find("input").props().value).toEqual("World");
+
+    wrapper.find("input").simulate("change", { target: { value: title } });
+    wrapper.find("button").simulate("click");
+
+    expect(wrapper.find("NotebookView").text()).toEqual(title);
+  });
 });
--- sun/src/components/App.tsx
+++ sun/src/components/App.tsx
@@ -1,25 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { AppState } from "../redux/store";
+import { notebookCreated } from "../redux/actions/virtualPapers";
+import { Notebook } from "../redux/actions/virtualPapers/types";
 
 function App() {
+  const notebooks = useSelector(
+    (state: AppState) => state.virtualPapers.notebooks
+  );
+
+  const dispatch = useDispatch();
+
+  const notebook: Notebook = {
+    id: 1,
+    title: "",
+    papers: [],
+  };
+
+  interface Props {
+    notebook: Notebook;
+  }
+
+  const NotebookView: React.FC<Props> = ({ notebook }) => (
+    <p>
+      <span>{notebook.title}</span>
+    </p>
+  );
+
+  const [title, setTitle] = useState("World");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <div style={{ color: "#000" }}>
+        {notebooks.length === 0 ? (
+          <span>Sem cadernos</span>
+        ) : (
+          notebooks.map((notebook) => (
+            <NotebookView key={notebook.id} notebook={notebook} />
+          ))
+        )}
+      </div>
+      <input
+        type="text"
+        style={{ textAlign: "center" }}
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          if (title.length > 0) {
+            notebook.title = title;
+            dispatch(notebookCreated(notebook));
+          }
+        }}
+      >
+        Criar Caderno
+      </button>
+    </main>
   );
 }
 
