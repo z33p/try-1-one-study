f4dee9ec5c231efed7bda063804224bc9775be21
z33p <raphael00fellipe@gmail.com>
2020-05-27 20:25:12 -0300

    Style(SUN): grid layout and 90vh height added

--- sun/src/components/MainRoutes.tsx
+++ sun/src/components/MainRoutes.tsx
@@ -3,13 +3,16 @@ import { Switch, Route } from "react-router-dom";
 import React from "react";
 import VirtualPapersPage from "./VirtualPapersPage";
 import PrivateRoute from "./PrivateRoute";
-import LoginPage from "./LoginPage";
+import LoginPage from "./LoginPage/LoginPage";
+import { Grid } from "@material-ui/core";
 
 const MainRoutes: React.FC = () => {
   return (
     <Switch>
-      <PrivateRoute exact path="/" component={VirtualPapersPage} />
-      <Route path="/login" component={LoginPage} />
+      <Grid container style={{ height: 90 + "vh" }}>
+        <PrivateRoute exact path="/" component={VirtualPapersPage} />
+        <Route path="/login" component={LoginPage} />
+      </Grid>
     </Switch>
   );
 };
